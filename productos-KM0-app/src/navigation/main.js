a9470bbaf39ddfec13e94d63e1d4f40e9afa2f5b
German Cuevas <german.1990.cuevas@gmail.com>
2022-08-15 19:29:46 -0300

    feat:producer and buyer registration screen are created

--- productos-KM0-app/src/navigation/main.js
+++ productos-KM0-app/src/navigation/main.js
@@ -1,16 +1,18 @@
 import React from "react";
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { ProductorRegister } from "../screens";
 import { NavigationContainer } from "@react-navigation/native";
+import CompradorRegister from "../screens/registro-comprador";
+import { ProductorRegister } from "../screens";
 
 const Stack = createNativeStackNavigator();
 
 const MainNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="ProductorRegister" component={ProductorRegister} />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Productor" component={CompradorRegister} />
+        {/*  <Stack.Screen name="Comprador" component={CompradorRegister} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
