09e7b9428f6a966fb45891ec464850f1ccf7c2c9
jmejiamu <jmejiamu@gmail.com>
2022-08-15 15:15:44 -0700

    update(remove changes): remove chages some else worked on this

--- productos-KM0-app/src/screens/registro-productor/styles.js
+++ productos-KM0-app/src/screens/registro-productor/styles.js
@@ -1,13 +1,3 @@
 import { StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({
-  container: {
-    marginHorizontal: 15,
-  },
-  input: {
-    width: "100%",
-  },
-  inputContainer: {
-    marginTop: 30,
-  },
-});
+export const styles = StyleSheet.create({});
