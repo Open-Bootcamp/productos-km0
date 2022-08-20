77d28d03c591daecd560782e5ef1411f5b809f95
Jose Aburto <77801995+PinoFlores@users.noreply.github.com>
2022-08-20 23:32:23 +0200

    configs: setups base models and migrations files

---
+++ productos-km0-api/database/factories/CalendarFactory.ts
@@ -0,0 +1,8 @@
+import Calendar from 'App/Models/Calendar'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+
+export default Factory.define(Calendar, ({ faker }) => {
+  return {
+    //
+  }
+}).build()
