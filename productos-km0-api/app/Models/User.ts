29e68354fd131ed6a9944240a6915ad8e11f9969
Jose Aburto <77801995+PinoFlores@users.noreply.github.com>
2022-08-20 16:05:30 +0200

    first commit

--- productos-km0-api/app/Models/User.ts
+++ productos-km0-api/app/Models/User.ts
@@ -1,16 +1,37 @@
 import { DateTime } from 'luxon'
 import Hash from '@ioc:Adonis/Core/Hash'
+import Database from '@ioc:Adonis/Lucid/Database'
 import { column, beforeSave, BaseModel } from '@ioc:Adonis/Lucid/Orm'
 
 export default class User extends BaseModel {
+  public static selfAssignPrimaryKey = true
+
   @column({ isPrimary: true })
   public id: number
+
+  @column({ serializeAs: null })
+  public fullname: string
+
+  @column({ serializeAs: null })
+  public username: string
+
+  @column({ serializeAs: null })
+  public password: string
 
   @column()
   public email: string
 
-  @column({ serializeAs: null })
-  public password: string
+  @column()
+  public address: string
+
+  @column()
+  public range_distance: number
+
+  @column()
+  public picture: string
+
+  @column()
+  public status: number
 
   @column()
   public rememberMeToken?: string
--- productos-km0-api/database/migrations/1660940960595_users.ts
+++ productos-km0-api/database/migrations/1660940960595_users.ts
@@ -6,8 +6,16 @@ export default class UsersSchema extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.string('email', 255).notNullable()
+
+      table.string('fullname', 255).notNullable()
+      table.string('username', 255).notNullable().unique()
       table.string('password', 180).notNullable()
+      table.string('email', 255).notNullable().unique()
+      table.point('location').notNullable()
+      table.double('range_distance').notNullable()
+      table.string('picture').nullable()
+      table.string('status').notNullable().defaultTo(1)
+
       table.string('remember_me_token').nullable()
 
       /**
