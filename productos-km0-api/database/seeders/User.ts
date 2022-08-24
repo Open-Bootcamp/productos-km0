3ab895d5c2ea6077c9e6b43429a724a4b0029b7c
unknown <Lautaro Ferreyra>
2022-08-24 14:19:03 -0300

    feat: refactor seed and preload users with role

--- productos-km0-api/database/seeders/User.ts
+++ productos-km0-api/database/seeders/User.ts
@@ -1,8 +1,6 @@
-import { DateTime } from 'luxon'
-import User from 'App/Models/User'
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import UserFactory from 'Database/factories/UserFactory'
-import Role from 'App/Models/Role'
+
 
 export default class extends BaseSeeder {
   public async run() {
@@ -11,29 +9,8 @@ export default class extends BaseSeeder {
     // Database.rawQuery('CREATE EXTENSION postgis')
 
 
-
-    const fakes = await UserFactory.createMany(10)
-
+    const fakes = await UserFactory.with('role', 1).createMany(10)
     await Promise.all(fakes.map((it) => it.save()))
 
-    const role = await Role.query().firstOrFail()
-
-    User.createMany([
-      {
-        id: '8c18be3f-9f06-4ec4-88f6-dffd09b536b3',
-        fullname: 'Maria Coin',
-        roleId: role.id,
-        username: 'mari0083',
-        password: '12345',
-        email: 'mary.coin@gmail.com',
-        address: '',
-        range_distance: 2000,
-        picture: '',
-        status: 1,
-        rememberMeToken: '',
-        createdAt: DateTime.now(),
-        updatedAt: DateTime.now(),
-      },
-    ])
   }
 }
