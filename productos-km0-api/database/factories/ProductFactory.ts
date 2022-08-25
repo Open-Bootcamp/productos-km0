e6c05f3ace61c447d33f6f26b5a856b59f3d4835
Jose Aburto <77801995+PinoFlores@users.noreply.github.com>
2022-08-25 20:01:25 +0200

    chore: adds fakes products

--- productos-km0-api/database/factories/ProductFactory.ts
+++ productos-km0-api/database/factories/ProductFactory.ts
@@ -1,8 +1,15 @@
 import Product from 'App/Models/Product'
 import Factory from '@ioc:Adonis/Lucid/Factory'
+import { DateTime } from 'luxon'
 
 export default Factory.define(Product, ({ faker }) => {
   return {
-    //
+    status: true,
+    name: faker.commerce.productName(),
+    description: faker.commerce.productName(),
+    picture: faker.image.food(),
+    category: faker.random.numeric(1),
+    createdAt: DateTime.now(),
+    updatedAt: DateTime.now(),
   }
 }).build()
