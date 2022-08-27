c753888420344ca68db56a07c76ec61f40796259
Jose Aburto <77801995+PinoFlores@users.noreply.github.com>
2022-08-27 21:09:43 +0200

    test: base review test

---
+++ productos-km0-api/tests/functional/reviews/create_review.spec.ts
@@ -0,0 +1,12 @@
+import { test } from '@japa/runner'
+import Database from '@ioc:Adonis/Lucid/Database'
+
+test.group('Reviews creation feature', (group) => {
+  // Write your test here
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+    return () => Database.rollbackGlobalTransaction()
+  })
+
+  test('blabla', async ({ client }) => {}).skip(true)
+})
