b5d7dc6d9ff11f6e63ed9a5f14b874d98c6346fe
Jose Aburto <77801995+PinoFlores@users.noreply.github.com>
2022-08-27 19:22:34 +0200

    ref: cleans up test impl

--- productos-km0-api/tests/functional/products/search.spec.ts
+++ productos-km0-api/tests/functional/products/search.spec.ts
@@ -1,58 +1,11 @@
 import { test } from '@japa/runner'
+import Database from '@ioc:Adonis/Lucid/Database'
 
-test.group('Products search', (group) => {
-  group.each.setup(() => {
-    console.log('executed before the test')
+test.group('Products search feature', (group) => {
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+    return () => Database.rollbackGlobalTransaction()
   })
 
-  group.each.teardown(() => {
-    console.log('executed after the test')
-  })
-
-  test('should fail on not coordinates present', async ({ client }) => {
-    const response = await client.get('/api/v1/products')
-
-    response.assertStatus(400)
-
-    response.assertBody({
-      errors: [
-        {
-          rule: 'required',
-          field: 'lat',
-          message: 'User latitude is required',
-        },
-        {
-          rule: 'required',
-          field: 'lng',
-          message: 'User longitud is required',
-        },
-      ],
-    })
-  })
-
-  test('should fail on invalid sent coordinates', async ({ client }) => {
-    const response = await client.get('/api/v1/products?lat=123&lng=3433')
-
-    response.assertStatus(400)
-
-    response.assertBody({
-      errors: [
-        {
-          rule: 'regex',
-          field: 'lat',
-          message: 'Invalid User latitude',
-        },
-        {
-          rule: 'regex',
-          field: 'lng',
-          message: 'Invalid User longitud',
-        },
-      ],
-    })
-  })
-
-  test('should return 200 on well coordinates', async ({ client }) => {
-    const response = await client.get('/api/v1/products?lat=47.04879993460173&lng=8.30724805994125')
-    response.assertStatus(200)
-  })
+  test('should return 200 on well coordinates', async ({ client }) => {})
 })
