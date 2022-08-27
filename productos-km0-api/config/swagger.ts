93de3febf9ded5e3060c31d567f6a662f6cd20f2
Jose Aburto <77801995+PinoFlores@users.noreply.github.com>
2022-08-27 23:09:11 +0200

    docs: setup swagger

---
+++ productos-km0-api/config/swagger.ts
@@ -0,0 +1,24 @@
+export default {
+  uiEnabled: true, //disable or enable swaggerUi route
+  uiUrl: 'docs', // url path to swaggerUI
+  specEnabled: true, //disable or enable swagger.json route
+  specUrl: '/swagger.json',
+
+  middleware: [], // middlewares array, for protect your swagger docs and spec endpoints
+
+  options: {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Productis KM0',
+        version: '1.0.0',
+        description: 'Application API',
+      },
+    },
+
+    apis: ['app/**/*.ts', 'docs/swagger/**/*.yml', 'start/**/*.ts'],
+    basePath: '/',
+  },
+  mode: process.env.NODE_ENV === 'production' ? 'PRODUCTION' : 'RUNTIME',
+  specFilePath: 'docs/swagger.json',
+}
