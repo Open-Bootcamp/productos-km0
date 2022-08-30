77a7407fcc8bae76310902d0a9bd74e692c7e14a
German Cuevas <german.1990.cuevas@gmail.com>
2022-08-30 13:21:37 -0300

    update: screen/index

--- productos-KM0-app/src/screens/index.js
+++ productos-KM0-app/src/screens/index.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 export * from './Buyer'
 export { default as ProductorRegister } from './registro-productor'
 export { default as CompradorRegister } from './registro-comprador'
 
-=======
-export { default as ProductorRegister } from "./registro-productor";
-export { default as CompradorRegister } from "./registro-comprador";
-export { default as RecoveryPass } from "./RecuperarPassword";
->>>>>>> aff193d73f26bc24e6d7a0169f190a41669997d7
+
