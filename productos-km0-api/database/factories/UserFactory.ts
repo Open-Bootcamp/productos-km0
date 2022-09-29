29328c35b36a25d6f70654eef2e0d02bcaffd68d
AMDevelopOB <amdevelop@open-bootcamp.com>
2022-09-29 15:54:18 +0200

    refactor: Unused imports

--- productos-km0-api/database/factories/UserFactory.ts
+++ productos-km0-api/database/factories/UserFactory.ts
@@ -5,27 +5,25 @@ import RoleFactory from './RoleFactory'
 import ProductFactory from './ProductFactory'
 
 export default function (type: number) {
-  return Factory.define(User, ({ faker }) => {
-    return {
-      status: true,
-      fullname: faker.name.fullName(),
-      username: faker.internet.userName(),
-      password: faker.internet.password(),
-      email: faker.internet.email(),
-      address: faker.address.direction(),
-      range_distance: faker.random.numeric(3),
-      picture: faker.image.people(),
-      roleId: type,
+  return Factory.define(User, ({ faker }) => ({
+    status: true,
+    fullname: faker.name.fullName(),
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    email: faker.internet.email(),
+    address: faker.address.direction(),
+    range_distance: faker.random.numeric(3),
+    picture: faker.image.people(),
+    roleId: type,
 
-      // This will give us a good range of area to find near products that match with a given searcher position
-      lat: parseFloat(faker.address.latitude(33, 32, 3)),
-      lng: parseFloat(faker.address.latitude(51, 50, 4)),
+    // This will give us a good range of area to find near products that match with a given searcher position
+    lat: parseFloat(faker.address.latitude(33, 32, 3)),
+    lng: parseFloat(faker.address.latitude(51, 50, 4)),
 
-      rememberMeToken: '',
-      createdAt: DateTime.now(),
-      updatedAt: DateTime.now(),
-    }
-  })
+    rememberMeToken: '',
+    createdAt: DateTime.now(),
+    updatedAt: DateTime.now(),
+  }))
     .relation('products', () => ProductFactory)
     .relation('role', () => RoleFactory)
     .build()
