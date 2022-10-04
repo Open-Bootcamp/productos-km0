2deafbcd4a8ec1b2e6c3dc78499f361ba73a6f82
David Morales <david.dmg89@gmail.com>
2022-10-04 19:58:00 +0200

    feat: add route

--- productos-km0-api/start/routes/transactions.routes.ts
+++ productos-km0-api/start/routes/transactions.routes.ts
@@ -2,4 +2,5 @@ import Route from '@ioc:Adonis/Core/Route'
 
 export default () => {
   Route.resource('transactions', 'TransactionController')
+  Route.post('/:id/transactions', 'TransactionController.store')
 }
